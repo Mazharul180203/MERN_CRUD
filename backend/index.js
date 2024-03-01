72022b379706bfcad5062d4ab42f32ebcb62992b
Md. Mazharul Islam <mazharul.saurav255@gmail.com>
2024-03-02 01:35:05 +0600

    CORS

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,41 @@
-import express from "express"
-import {PORT,MONGO_URL} from "./config.js"
+import express from "express";
+import { PORT, MONGO_URL } from "./config.js";
 import mongoose from "mongoose";
-import booksRoute from './routes/bookRoutes.js'
+import booksRoute from "./routes/bookRoutes.js";
+import cors from "cors";
 
 const app = express();
 
-
 // Middleware for parsing request body
 app.use(express.json());
-app.get('/',(req, res)=>{
 
-    return res.send('welcome saurav');
-})
+//Middleware can handle CORS policy
+//Option 1: Allow all the origins with default of cors(*)
 
-app.use('/books',booksRoute);
+app.use(cors());
+//Option 2: Allow custom origins with default of cors(*)
+// app.use(
+//   cors({
+//       origin: "",
+//       methods: ["GET", "POST", "PUT", "DELETE"],
+//       allowedHeaders:['Content-Type'],
+//   }),
+// );
 
+app.get("/", (req, res) => {
+  return res.send("welcome saurav");
+});
 
+app.use("/books", booksRoute);
 
 mongoose
-    .connect(MONGO_URL)
-    .then(()=>{
-        console.log('App run successfully');
-        app.listen(PORT,()=>{
-            console.log(`app port : ${PORT}`);
-        })
-    }).catch((e)=>{
-        console.log("App destroyed",e);
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("App run successfully");
+    app.listen(PORT, () => {
+      console.log(`app port : ${PORT}`);
     });
-
+  })
+  .catch((e) => {
+    console.log("App destroyed", e);
+  });
